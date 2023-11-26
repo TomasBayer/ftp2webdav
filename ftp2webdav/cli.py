e9735abe659a287f6bf5c21c43a6a13750077e36
Tomas Bayer <mail@tomas.berlin>
2023-11-26 23:02:27 +0100

    Refactor the CLI with typer

--- ftp2webdav/cli.py
+++ ftp2webdav/cli.py
@@ -1,48 +1,52 @@
-import argparse
 import logging
-import sys
 from pathlib import Path
+from typing import Annotated, Optional
 
+import typer
 import yaml
+from click import ClickException
 
 from ftp2webdav.config import build_configuration
 from ftp2webdav.server import FTP2WebDAV
 
+app = typer.Typer()
 
-def build_argparser():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--config-file', '-c', type=Path, help="config file")
-    parser.add_argument('--verbose', '-v', action='store_true', help="verbose mode")
-
-    return parser
+DEFAULT_CONFIG_FILE_PATHS = (
+    Path("~/.ftp2webdav.conf").expanduser(),
+    Path("/etc/ftp2webdav.conf"),
+)
 
 
-def run():
-    parser = build_argparser()
-    args = parser.parse_args()
-
-    log_level = logging.DEBUG if args.verbose else logging.INFO
+@app.command()
+def run(
+        *,
+        config_file: Annotated[Optional[Path], typer.Option(
+            "--config", "-c",
+            help="Path to config file.",
+        )] = None,
+        verbose: Annotated[bool, typer.Option(
+            "--verbose", "-v",
+            help="Verbose output.",
+        )] = False,
+):
+    log_level = logging.DEBUG if verbose else logging.INFO
     logging.basicConfig(level=log_level, format='%(asctime)-15s %(levelname)-8s %(message)s')
 
     # Find config file
-    config_file = None
-    if args.config_file:
-        config_file = args.config_file.expanduser()
-    else:
-        for file in (Path("~/.ftp2webdav.conf").expanduser(), Path("/etc/ftp2webdav.conf")):
+    if config_file is None:
+        for file in DEFAULT_CONFIG_FILE_PATHS:
             if file.exists():
                 config_file = file
                 break
 
-    if config_file is None:
-        sys.exit("No configuration file found.")
+    if config_file is None or not config_file.exists():
+        raise ClickException("No configuration file found.")
 
     try:
         with config_file.open() as fh:
             config = build_configuration(yaml.safe_load(fh))
-    except yaml.YAMLError:
-        sys.exit(f"Invalid YAML in configuration file: {config}")
+    except yaml.YAMLError as err:
+        raise ClickException(f"Invalid YAML in configuration file: {config}") from err
 
     ftp = FTP2WebDAV(
         webdav_config=config['webdav'],
